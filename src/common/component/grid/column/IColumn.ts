294a665292d580fbfec8852fe64c6e4cec8397fa
apoterenko <a.poterenko@gmail.com>
2016-03-04 19:08:13 +0300

    *improve IColumn interface

--- src/common/component/grid/column/IColumn.ts
+++ src/common/component/grid/column/IColumn.ts
@@ -3,4 +3,10 @@ import {IModel} from "../../../data/model/IModel";
 export interface IColumn {
 
     getModelValue(model:IModel):string;
+
+    getDescription():string;
+
+    setName(name:string):IColumn;
+
+    setDescription(description:string):IColumn;
 }
