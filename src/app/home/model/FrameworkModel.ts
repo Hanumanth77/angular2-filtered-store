fd330497fbfd1310917be128b6cf5db4ff181579
apoterenko <a.poterenko@gmail.com>
2016-03-04 18:50:24 +0300

    *Fixed FrameworkModel

--- src/app/home/model/FrameworkModel.ts
+++ src/app/home/model/FrameworkModel.ts
@@ -3,7 +3,7 @@ import {Model} from "../../../common/data/model/Model";
 export class FrameworkModel extends Model {
 
     constructor(public name:string,
-                public description:string,
+                public counter:number,
                 public released:Date) {
         super();
     }
